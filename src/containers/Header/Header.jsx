f8e8f85b32688658aa57b513b90ffd6be04bf232
gmluqa <gmluqa@protonmail.com>
2023-01-01 12:23:19 +0200

    chore: Got sass and react-bootstrap working together nicely

---
+++ src/containers/Header/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <div>
+      <div>Logo</div>
+      <div>Search</div>
+      <div>Cart</div>
+    </div>
+  );
+};
+
+export default Header;
