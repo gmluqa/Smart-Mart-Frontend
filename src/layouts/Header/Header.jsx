6c2dce1fd2e245f2533ac6eff046e8107f32278a
gmluqa <gmluqa@protonmail.com>
2023-01-03 12:08:55 +0200

    feat: navbar is responsive, logo is transparent

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,18 +1,37 @@
-import React from "react";
-
 import "../../App.scss";
 import "./Header.scss";
 import logo from "../../assets/img/header/SmartMartLogo.png";
+import { Container } from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 const Header = () => {
   return (
-    <div className="d-flex justify-content-between p-5 ">
-      <img src={logo}></img>
-      <div>Search </div>
-      <div>Login</div>
-      <div>Cart</div>
-    </div>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">
+          <img src={logo} className="logo"></img>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link>Search</Nav.Link>
+            <Nav.Link href="/cart">Cart</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
 export default Header;
+
+// <Container>
+//         <Navbar.Brand href="/">
+//           <img src={logo} className="logo"></img>
+//         </Navbar.Brand>
+//         <div>Search</div>
+//         <div>Login</div>
+//         <div>Cart</div>
+//       </Container>
