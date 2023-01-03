4c8a44bc0d4dbd03354a2d8a760d028bae06f8ee
gmluqa <gmluqa@protonmail.com>
2023-01-03 19:01:44 +0200

    chore: added endpoint, axios, now just need to call it correctly

---
+++ src/services/apiCalls/getProductAndImgByDefiner.js
@@ -0,0 +1,20 @@
+import { apiRoute } from "./apiConfig.js";
+import axios from "axios";
+
+let productNameAndImgPath = { product_name: "", img_path: "" };
+
+export async function getProductNameAndImgByDefiner(id, definer) {
+  try {
+    await axios.get(`${apiRoute}/product/id/${id}`).then(response => {
+      productNameAndImgPath.product_name = response.data.product_name;
+    });
+    await axios
+      .get(`${apiRoute}/product/id/${id}/img/${definer}`)
+      .then(response => {
+        productNameAndImgPath.img_path = response.data.img_path;
+      });
+    return productNameAndImgPath;
+  } catch (error) {
+    return productNameAndImgPath;
+  }
+}
