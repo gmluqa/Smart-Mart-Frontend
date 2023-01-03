1abbe1dcc25013b5913e409918bba9267a403c8f
gmluqa <gmluqa@protonmail.com>
2023-01-03 12:43:24 +0200

    chore: react-router-dom added succesfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Header from "./layouts/Header/Header";
+import { Route, Routes } from "react-router-dom";
 import "./App.scss";
+import IndexPage from "./pages/IndexPage/IndexPage";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<IndexPage />} />
+      </Routes>
     </div>
   );
 }
