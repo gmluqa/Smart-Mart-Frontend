ce67bd1879dd7a226e26ccfce01046a52e7310e4
gmluqa <gmluqa@protonmail.com>
2023-01-09 10:55:59 +0200

    chore: indexpage rough finalized

--- src/components/ProductImage/ProductImage.jsx
+++ src/components/ProductImage/ProductImage.jsx
@@ -1,19 +1,32 @@
 import { Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./ProductImage.scss";
+import getProductByName from "../../services/apiCalls/getProductByName";
 
 const ProductImage = props => {
+  const [apiCall, setApiCall] = useState({});
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getProductByName(props.name).then(value => setApiCall(value));
+  }, []);
+
   return (
-    <Col className="text-center">
+    <Col
+      onClick={() => navigate(`/products/${props.name}`)}
+      className="text-center"
+      style={{ cursor: "pointer" }}
+    >
       <div className="productImageBorder">
-        <a className="productLink" href={"./products/" + props.name}>
+        <div className="productLink" href={"./products/" + props.name}>
           <div className="text-border-name">{props.name}</div>
-        </a>
+        </div>
         <img
           className="productImageImage"
-          src={props.url}
+          src={apiCall?.[2]?.[0]?.img_path}
           alt={props.name + " img"}
         />
       </div>
