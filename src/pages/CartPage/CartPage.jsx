337640bafe8f63826280794f9080c03c63605666
gmluqa <gmluqa@protonmail.com>
2023-01-13 16:58:55 +0200

    chore: removed annoying log

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -14,7 +14,6 @@ const IndexPage = () => {
     setCartItemsArray(JSON.parse(localStorage.getItem("SmartMartCart")));
   }, []);
 
-  console.log(cartItemsArray);
   return (
     <Container fluid>
       <Row>
