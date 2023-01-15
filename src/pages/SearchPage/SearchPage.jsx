f3517238946aaeae37785f16dc2ca794eb922980
gmluqa <gmluqa@protonmail.com>
2023-01-15 18:34:00 +0200

    feat: search finished and looking good

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,14 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { searchProduct } from "../../services/apiCalls/searchProduct";
+import "./SearchPage.scss";
 
 const SearchPage = () => {
   const [criteria, setCriteria] = useState("");
-  const [results, setResults] = useState({});
+  const [results, setResults] = useState([]);
+  const navigate = useNavigate();
+
+  const renderAllResults = () => {
+    return results.map((result, index) => {
+      return (
+        <Col
+          className="searchedItem"
+          key={index}
+          onClick={() => navigate(`../products/${result.product_name}`)}
+        >
+          {result.product_name}
+        </Col>
+      );
+    });
+  };
 
   const onChange = e => {
     setCriteria(e.target.value);
   };
-  console.log(criteria);
+
+  useEffect(() => {
+    if (criteria !== "") {
+      const bring = setTimeout(() => {
+        searchProduct(criteria)
+          .then(res => {
+            setResults(res);
+            return res;
+          })
+          .catch(error => error);
+      }, 350);
+
+      return () => clearTimeout(bring);
+    } else {
+      setResults({});
+    }
+  }, [criteria]);
 
   return (
     <Container>
@@ -16,6 +50,7 @@ const SearchPage = () => {
         <Row>
           <input type="text" onChange={onChange} />
         </Row>
+        {results?.[0] ? renderAllResults() : null}
       </Col>
     </Container>
   );
